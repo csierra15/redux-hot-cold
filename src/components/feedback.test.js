c219445b5fa531a9aa5bd2d297de3b632312d7b0
Carly Sierra <carlylwomack@gmail.com>
2018-02-15 14:05:47 -0600

    imported unconnected components -- all tests pass

--- src/components/feedback.test.js
+++ src/components/feedback.test.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 
-import Feedback from './feedback';
+import {Feedback} from './feedback';
 
 describe('<Feedback />', () => {
     it('Renders without crashing', () => {
